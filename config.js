4db7d51b59084aa3713d912957fe8871934f20ba
Amrit <116807282+AmritSaha123@users.noreply.github.com>
2023-08-07 14:25:41 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcEm9i4VAR_8Vk040WGr4ZXi00Ff0-At0",
+  authDomain: "storytelling-app-b33a5.firebaseapp.com",
+  databaseURL: "https://storytelling-app-b33a5-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-b33a5",
+  storageBucket: "storytelling-app-b33a5.appspot.com",
+  messagingSenderId: "8451995746",
+  appId: "1:8451995746:web:f0b44c4e5dc8fd7026393b"
+};
